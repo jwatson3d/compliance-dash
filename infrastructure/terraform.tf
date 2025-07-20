9e6a00cb976457ce620bdb6d244a2c01e5514ad7
John Watson <jwatson3d@gmail.com>
2025-07-20 16:29:10 -0400

    chore: use newer provider versions

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.5"
-
+  required_version = ">= 1.10.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.100"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      version = "~> 3.6"
     }
   }
 }
